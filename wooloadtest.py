8ae89c31fd29aeabfbd585d1c0182cacde4b092e
harshadyeola <harshad.yeola@rtcamp.com>
2015-12-29 16:00:42 +0530

    Used woocommerce API

--- wooloadtest.py
+++ wooloadtest.py
@@ -1,34 +1,78 @@
 from locust import HttpLocust, TaskSet, task
-import string 
-import random
-import time
+from woocommerce import API
 
+class WooLocust(HttpLocust):
+    """
+    This is the abstract Locust class which should be subclassed. It provides an XML-RPC client
+    that can be used to make XML-RPC requests that will be tracked in Locust's statistics.
+    """
+    def __init__(self):
+        self.client = API(
+            url="http://dev.linuxontheheads.com",
+            consumer_key="ck_84946169aed02894f3f53483432c339605f997a0",
+            consumer_secret="cs_f9fbd218a9618201ae1aa841f19c0435f1a41440"
+            )
 class UserBehavior(TaskSet):
     def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled """
-        #self.comment()
+        pass
 
     @task(1)
-    def comment(self):
-        from splinter import Browser
+    def createorder(self):
+        data = {
+            "order": {
+                "payment_details": {
+                    "method_id": "bacs",
+                    "method_title": "Direct Bank Transfer",
+                    "paid": True
+                },
+                "billing_address": {
+                    "first_name": "John",
+                    "last_name": "Doe",
+                    "address_1": "969 Market",
+                    "address_2": "",
+                    "city": "San Francisco",
+                    "state": "CA",
+                    "postcode": "94103",
+                    "country": "US",
+                    "email": "john.doe@example.com",
+                    "phone": "(555) 555-5555"
+                },
+                "shipping_address": {
+                    "first_name": "John",
+                    "last_name": "Doe",
+                    "address_1": "969 Market",
+                    "address_2": "",
+                    "city": "San Francisco",
+                    "state": "CA",
+                    "postcode": "94103",
+                    "country": "US"
+                },
+                "customer_id": 2,
+                "line_items": [
+                    {
+                        "product_id": 56,
+                        "quantity": 2
+                    },
+                    {
+                        "product_id": 70,
+                        "quantity": 1,
+                        "variations": {
+                            "pa_color": "Black"
+                        }
+                    }
+                ],
+                "shipping_lines": [
+                    {
+                        "method_id": "flat_rate",
+                        "method_title": "Flat Rate",
+                        "total": 10
+                    }
+                ]
+            }
+        }
+        self.client.post("/orders", data)
 
-        with Browser('phantomjs') as browser:   	  
-	    foo = ['70', '37', '53', '31', '56', '50' , '19', '73', '76', '93', '99', '47', '34', '79', '60', '15', '96', '90', '87', '83', '67']
-	# Visit URL
-            url = WebsiteUser.host + "shop/?add-to-cart=" + random.choice(foo)
-            browser.visit(url)
-	    url = WebsiteUser.host + "checkout/"
-            browser.visit(url)
- #           browser.fill('billing_email', 'hello@dfdf.dfdfdfdf')
-    # Find and click the 'Place Order' button
-            button = browser.find_by_name('woocommerce_checkout_place_order')
-    # Interact with elements
-            button.click()
-
-#	    browser.quit()
-class WebsiteUser(HttpLocust):
-    host = "http://wootest.rtcamp.net/"
-    weight = 1
+class ApiUser(WooLocust):
+    host = "http://dev.linuxontheheads.com"
     task_set = UserBehavior
-#    min_wait=5000
-#    max_wait=9000
+    min_wait=1
